97dcc88c12a85eceb7b97cebcea07f345ca9cc9a
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-10-24 12:48:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,13 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
     
-    # URL облачной функции Yandex Cloud
-    OPEN_LIBRARY_FUNCTION_URL = os.getenv(
-        'OPEN_LIBRARY_FUNCTION_URL', 
-        'https://functions.yandexcloud.net/your-function-id'
+    # Yandex API Gateway URL
+    OPEN_LIBRARY_API_GATEWAY_URL = os.getenv(
+        'OPEN_LIBRARY_API_GATEWAY_URL', 
+        'https://<api-gateway-id>.apigw.yandexcloud.net/open-library/search'
     )
-
+    
+    # Yandex SmartCaptcha
     SMARTCAPTCHA_SERVER_KEY = os.getenv('SMARTCAPTCHA_SERVER_KEY', '')
     SMARTCAPTCHA_CLIENT_KEY = os.getenv('SMARTCAPTCHA_CLIENT_KEY', '')
     
